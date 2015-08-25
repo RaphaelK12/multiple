e8503bf2965339e5dda09712a76436146981602a
Anton Rogazinskii <Rogazinskii@mail.ru>
2015-08-25 22:40:39 +0600

    defs fix

--- color_converter/target_defs.h
+++ color_converter/target_defs.h
@@ -7,7 +7,7 @@
 #define TARGET_FREEALIGN free
 #elif defined WINDOWS_BUILD
 #define TARGET_INLINE __forceinline
-#define TARGET_MEMALIGN(align, size) memalign( (size), (align) )
+#define TARGET_MEMALIGN(align, size) _aligned_malloc( (size), (align) )
 #define TARGET_FREEALIGN _aligned_free
 #endif
 
